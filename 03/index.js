dde8cee9f534fce051c9a7c78d7ffef3a1934702
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-21 18:07:07 -0300

    metodo imprimirDetalhes implementado

--- 03/index.js
+++ 03/index.js
@@ -1,18 +1,3 @@
-function addProdutoAoCarrinho(carrinho, produto) {
-    let indiceProdutoExistente = -1;
-    for (let i = 0; i < carrinho.produtos.length; i++) {
-        if (carrinho.produtos[i].id === produto.id) {
-            indiceProdutoExistente = i;
-            break;
-        }
-    }
-    if (indiceProdutoExistente === -1) {
-        carrinho.produtos[carrinho.produtos.length] = produto;
-    } else {
-        carrinho.produtos[indiceProdutoExistente].qtd += produto.qtd;
-    }
-
-}
 const carrinho = {
     nomeDoCliente: "Guido Bernal",
     produtos: [
@@ -41,8 +26,44 @@ const carrinho = {
         console.log(`Cliente: ${this.nomeDoCliente}
 Total de intens: ${totalItens};
 Total a pagar: RS${totalPagarTexto}`);
-    }
+    },
+
+    addProduto: function (carrinho, produto) {
+        let indiceProdutoExistente = -1;
+        for (let i = 0; i < carrinho.produtos.length; i++) {
+            if (carrinho.produtos[i].id === produto.id) {
+                indiceProdutoExistente = i;
+                break;
+            }
+        }
+        if (indiceProdutoExistente === -1) {
+            carrinho.produtos[carrinho.produtos.length] = produto;
+        } else {
+            carrinho.produtos[indiceProdutoExistente].qtd += produto.qtd;
+        }
+
+    },
+
+    imprimirDetalhes: function () {
+        console.log(this.nomeDoCliente);
+        for (let items = 0; items < this.produtos.length; items++) {
+            console.log(`Item ${items + 1} - ${this.produtos[items].nome} - ${this.produtos[items].qtd} und - R$ ${this.produtos[items].precoUnit / 100 * this.produtos[items].qtd} `)
+        }
+        console.log();
+
+        let totalItens = 0;
+        let totalPagar = 0;
+        for (let itens of carrinho.produtos) {
+            totalItens += itens.qtd;
+            totalPagar += itens.qtd * itens.precoUnit;
+        }
+        let totalPagarTexto = totalPagar / 100;
+        console.log(`Total de intens: ${totalItens};
+Total a pagar: RS${totalPagarTexto}`);
+    },
+
 }
+
 
 const novaBermuda = {
     id: 2,
@@ -51,5 +72,6 @@ const novaBermuda = {
     precoUnit: 5000
 }
 
-addProdutoAoCarrinho(carrinho, novaBermuda);
-carrinho.imprimirResumo();
+carrinho.addProduto(carrinho, novaBermuda);
+//carrinho.imprimirResumo();
+carrinho.imprimirDetalhes();
