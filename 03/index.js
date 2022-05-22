30edaef706f39fb1528dfbe34561c97d68e1b5f8
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-22 11:44:39 -0300

    exercicio 03 feito

--- 03/index.js
+++ 03/index.js
@@ -16,55 +16,78 @@ const carrinho = {
     ],
 
     imprimirResumo: function () {
-        let totalItens = 0;
-        let totalPagar = 0;
-        for (let itens of carrinho.produtos) {
-            totalItens += itens.qtd;
-            totalPagar += itens.qtd * itens.precoUnit;
-        }
-        let totalPagarTexto = totalPagar / 100;
         console.log(`Cliente: ${this.nomeDoCliente}
-Total de intens: ${totalItens};
-Total a pagar: RS${totalPagarTexto}`);
+Total de intens: ${this.calcularTotalDeItens()};
+Total a pagar: RS${this.calcularTotalAPagar()}`);
     },
 
-    addProduto: function (carrinho, produto) {
+    addProduto: function (produto) {
         let indiceProdutoExistente = -1;
-        for (let i = 0; i < carrinho.produtos.length; i++) {
-            if (carrinho.produtos[i].id === produto.id) {
+        for (let i = 0; i < this.produtos.length; i++) {
+            if (this.produtos[i].id === produto.id) {
                 indiceProdutoExistente = i;
                 break;
             }
         }
         if (indiceProdutoExistente === -1) {
-            carrinho.produtos[carrinho.produtos.length] = produto;
+            this.produtos[this.produtos.length] = produto;
         } else {
-            carrinho.produtos[indiceProdutoExistente].qtd += produto.qtd;
+            this.produtos[indiceProdutoExistente].qtd += produto.qtd;
         }
 
     },
 
     imprimirDetalhes: function () {
         console.log(this.nomeDoCliente);
-        for (let items = 0; items < this.produtos.length; items++) {
-            console.log(`Item ${items + 1} - ${this.produtos[items].nome} - ${this.produtos[items].qtd} und - R$ ${this.produtos[items].precoUnit / 100 * this.produtos[items].qtd} `)
+        console.log();
+        for (let itens = 0; itens < this.produtos.length; itens++) {
+            console.log(`Item ${itens + 1} - ${this.produtos[itens].nome} - ${this.produtos[itens].qtd} und - R$ ${this.produtos[itens].precoUnit / 100 * this.produtos[itens].qtd} `);
         }
         console.log();
+        console.log(`Total de intens: ${this.calcularTotalDeItens()}
+Total a pagar: RS${this.calcularTotalAPagar() / 100}`);
+    },
 
+    calcularTotalDeItens: function () {
         let totalItens = 0;
-        let totalPagar = 0;
-        for (let itens of carrinho.produtos) {
+        for (let itens of this.produtos) {
             totalItens += itens.qtd;
+        }
+        return totalItens;
+    },
+
+    calcularTotalAPagar: function () {
+        let totalPagar = 0;
+        for (let itens of this.produtos) {
             totalPagar += itens.qtd * itens.precoUnit;
         }
         let totalPagarTexto = totalPagar / 100;
-        console.log(`Total de intens: ${totalItens};
-Total a pagar: RS${totalPagarTexto}`);
+        return totalPagar;
     },
 
+    calcularDesconto: function () {
+        let totalPagar = this.calcularTotalAPagar();
+        let totalItens = this.calcularTotalDeItens();
+
+        let descontoPorItens = 0;
+        let descontoPorTotal = 0;
+
+        if (totalItens > 4) {
+            descontoPorItens = this.produtos[0].precoUnit;
+            for (let i = 1; i < this.produtos.length; i++) {
+                if (this.produtos[i].precoUnit < descontoPorItens)
+                    descontoPorItens = this.produtos[i].precoUnit;
+            }
+        }
+
+        if (totalPagar > 10000) {
+            descontoPorTotal = totalPagar * 0.1;
+        }
+
+        return descontoPorItens > descontoPorTotal ? descontoPorItens : descontoPorTotal;
+    }
 }
-
-
+console.log(carrinho.calcularDesconto() / 100);
 const novaBermuda = {
     id: 2,
     nome: "Bermuda",
@@ -72,6 +95,17 @@ const novaBermuda = {
     precoUnit: 5000
 }
 
-carrinho.addProduto(carrinho, novaBermuda);
-//carrinho.imprimirResumo();
+carrinho.addProduto(novaBermuda);
+
+const novoTenis = {
+    id: 3,
+    nome: "Tenis",
+    qtd: 1,
+    precoUnit: 10000
+}
+carrinho.addProduto(novoTenis);
+
+console.log(carrinho.calcularDesconto() / 100);
+
+carrinho.imprimirResumo();
 carrinho.imprimirDetalhes();
